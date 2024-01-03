58c64eac3d5e9739fb1c7ccafe005428250a57c5
Mert Salik <53333294+codermert@users.noreply.github.com>
2024-01-03 19:40:23 +0300

    Update eczane2.js

--- eczane2.js
+++ eczane2.js
@@ -4,8 +4,23 @@ const fs = require('fs');
 
 async function scrapePharmacies(url, cityName) {
   const headers = {
-    'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36',
+    'Host': 'www.eczaneler.gen.tr',
+    'method': 'GET',
+    'scheme': 'https',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'tr-TR,tr;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Cookie': '_gid=GA1.3.973818948.1704299672; _gat_gtag_UA_533858_20=1; _ga_RTECLYJVM5=GS1.1.1704299672.11.0.1704299672.0.0.0; _ga=GA1.1.48944175.1691683467',
+    'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Linux"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     Referer: url,
   };
 
