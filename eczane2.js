226a498d2f06f588f9b603b7fb2525ea4194f70e
Mert Salık <53333294+codermert@users.noreply.github.com>
2024-07-20 14:46:39 +0300

    Update eczane2.js

--- eczane2.js
+++ eczane2.js
@@ -1,33 +1,28 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const cloudscraper = require('cloudscraper');
 
 async function scrapePharmacies(url, cityName) {
-  const headers = {
-    'Host': 'www.eczaneler.gen.tr',
-    'method': 'GET',
-    'scheme': 'https',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'tr-TR,tr;q=0.9',
-    'Cache-Control': 'max-age=0',
-    'Cookie': '_gid=GA1.3.973818948.1704299672; _gat_gtag_UA_533858_20=1; _ga_RTECLYJVM5=GS1.1.1704299672.11.0.1704299672.0.0.0; _ga=GA1.1.48944175.1691683467',
-    'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-    'Sec-Ch-Ua-Mobile': '?0',
-    'Sec-Ch-Ua-Platform': '"Linux"',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    Referer: url,
+  const options = {
+    uri: url,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+      'Accept-Language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1'
+    },
+    cloudflareTimeout: 1000,
+    cloudflareMaxTimeout: 2000,
+    followAllRedirects: true,
+    jar: true // enable cookies
   };
 
   try {
-    const response = await axios.get(url, { headers });
-    const html = response.data;
-    const $ = cheerio.load(html);
+    const response = await cloudscraper(options);
+    const $ = cheerio.load(response);
 
     const eczaneVerileri = [];
 
@@ -45,9 +40,11 @@ async function scrapePharmacies(url, cityName) {
       const pharmacyUrl = $(element).find('.col-lg-3 a').attr('href');
       const individualPageUrl = `https://www.eczaneler.gen.tr${pharmacyUrl}`;
 
-      const individualPageResponse = await axios.get(individualPageUrl, { headers });
-      const individualPageHtml = individualPageResponse.data;
-      const $individualPage = cheerio.load(individualPageHtml);
+      // Rastgele bekleme süresi
+      await new Promise(resolve => setTimeout(resolve, Math.random() * 3000 + 1000));
+
+      const individualPageResponse = await cloudscraper({ ...options, uri: individualPageUrl });
+      const $individualPage = cheerio.load(individualPageResponse);
 
       const googleMapsUrl = $individualPage('a[href^="https://www.google.com/maps?"]').attr('href');
 
